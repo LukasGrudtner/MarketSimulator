140939a1649d276ec0a2f8b35fe6aa14adf4888c
LuizFelipeBaroncello <luizfelipe_rv97@hotmail.com>
2016-09-26 19:16:06 -0300

    documentação

--- Projetos/Projeto1/Market/include/circular_list.h
+++ Projetos/Projeto1/Market/include/circular_list.h
@@ -6,35 +6,120 @@
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 
+//! Classe CircularList para uma Lista.
+/*!
+*   Na classe CircularList, e implementado o funcionamento de uma lista
+*   atraves de Nodes que referenciam o proximo valor na lista e o dado na mesma.
+*/
 class circular_list
 {
  public:
+        /*!
+        *   Construtor.
+        */
         CircularList();
 
+        /*!
+        *   Destrutor.
+        *   \sa clear()
+        */
+        ~CircularList();
+
+        /*!
+        *   Limpa a lista.
+        *   \sa empty(), pop_front()
+        */
         void clear();
 
+        /*!
+        *   Adiciona fila no final.
+        *   \param data dado a ser adicionado.
+        *   \sa insert()
+        */
         void push_back(const T& data);
+
+        /*!
+        *  Adiciona fila no começo.
+        *   \param data dado a ser adicionado.
+        *   \sa empty()
+        */
         void push_front(const T& data);
+
+        /*!
+        *   Insere dado em uma posicao n.
+        *   \param data dado a ser adicionado.
+        *   \param index posicao q sera adicionado.
+        *   \sa push_front
+        */
         void insert(const T& data, std::size_t index);
+
+        /*!
+        *   Insere dado de forma ordenada.
+        *   \param data dado a ser adicionado
+        *   \sa insert(), push_front()
+        */
         void insert_sorted(const T& data);
 
-        T& at(std::size_t index);
+        /*!
+        *   Retorna dado.
+        *   \param index posicao do dado a ser retornado.
+        *   \return dado.
+        */
         const T& at(std::size_t index) const;
 
+        /*!
+        *   Retira e retorna dado.
+        *   \param posicao do dado a ser retirado.
+        *   \return dado retirado.
+        *   \sa pop_front()
+        */
         T pop(std::size_t index);
+
+        /*!
+        *   Retira e retorna dado do final.
+        *   \return dado retirado.
+        *   \sa pop()
+        */
         T pop_back();
+
+        /*!
+        *   Retira e retorna dado do inicio.
+        *   \return dado retirado.
+        */
         T pop_front();
+
+        /*!
+        *   Remove dado da lista.
+        *   \param data dado a ser removido.
+        */
         void remove(const T& data);
 
+        /*!
+        *   Retorna true se a lista estiver vazia.
+        *   \return uma variavel booleana.
+        */
         bool empty() const;
-        bool contains(const T& data) const;
+
+        /*!
+        *   Retorna true se o dado recebido esta contido na lista.
+        *   \param data dado a ser verificado.
+        *   \return uma variavel booleana.
+        */
+        bool contains(const T& data) const
+
+        /*!
+        *   Retorna posicao de uma variavel.
+        *   \param data variavel a ser verificada.
+        *   \return um inteiro correspondente a posicao.
+        */;
         std::size_t find(const T& data) const;
 
+        /*!
+        *   Retorna a quantidade de elementos na lista.
+        */
         std::size_t size() const;
-        std::size_t smaller_queue() const;
-        std::size_t smaller_amount_of_products() const;
 
     private:
 //! Classe Node para referenciar dado e proximo elemento na fila.
