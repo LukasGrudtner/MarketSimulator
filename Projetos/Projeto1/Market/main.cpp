1b0e858d3c22098acd4c4026cdfa8cd60ea0269c
Lukas Grüdtner <lukasgrudtner@hotmail.com>
2016-10-11 19:52:00 -0300

    Main limpo.

--- Projetos/Projeto1/Market/main.cpp
+++ Projetos/Projeto1/Market/main.cpp
@@ -14,27 +14,12 @@ using namespace std;
 
 int main()
 {
+    using namespace read;
 
+    Control* control = new Control();
 
+    //TestLuiz* testes = new TestLuiz();
+    //Lukas* lks = new Lukas();
 
-            TestLuiz* testes = new TestLuiz();
-      //      Lukas* lks = new Lukas();
-
-    //using namespace read;
-    //std::ifstream ss{"input.txt"};
-    //std::ifstream ss;
-    //ss.open("input.dat");
-
-
-    //Control* user = new Control();
-    //user->read_market(ss);
-
-    // Fecha o arquivo
-    //ss.close();
-
-    //cout << "Market Name: " << user->get_market_name() << "\n"
-     //    << "Time of Simulation (in hours): " << user->get_time_of_simulation_in_hours() << "\n"
-     //    << "Average Arrival Time of Customers (in seconds): " << user->get_average_arrival_time_of_customers_in_seconds() << "\n"
-     //    << "Number of Market Box: " << user->get_number_of_market_box() << "\n";
 
 }
